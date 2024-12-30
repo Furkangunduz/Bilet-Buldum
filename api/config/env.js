270080e2f62b2cc82989dee33f90836a86f6310c
Furkan Gündüz <furkan.gundz00@gmail.com>
2024-12-30 08:42:09 +0300

    Add Postman collection for Bilet Buldum API and remove unused API files
    - Added a new Postman collection file for API testing and documentation.
    - Removed several unused files from the API, including environment variables, database keys, main server file, and various utility classes and configurations.
    - Cleaned up the project structure by deleting unnecessary files to streamline the codebase.

--- api/config/env.js
+++
@@ -1,7 +0,0 @@
-module.exports = function startEnv() {
-  try {
-    require("dotenv").config();
-  } catch (error) {
-    console.log("Error trying to start env => ", error);
-  }
-};
--- new-api/src/config/env.ts
+++ new-api/src/config/env.ts
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
 import { z } from 'zod';
+
+dotenv.config({ path: '.env' });
 
 const envSchema = z.object({
   PORT: z.string().transform(Number).pipe(z.number().positive()),
