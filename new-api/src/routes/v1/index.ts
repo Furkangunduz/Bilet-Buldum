bb46539fec94252b2f8c53e440750e09aad3af75
Furkan Gündüz <furkan.gundz00@gmail.com>
2025-01-05 13:55:57 +0300

    Add healthcheck route to API

--- new-api/src/routes/v1/index.ts
+++ new-api/src/routes/v1/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import authRoutes from './auth.routes';
 import contactRoutes from './contact.routes';
 import crawlerRoutes from './crawler.routes';
+import healthcheckRoutes from './healthcheck.routes';
 import searchAlertRoutes from './search-alerts.routes';
 import tcddRoutes from './tcdd.routes';
 
@@ -12,5 +13,6 @@ router.use('/crawler', crawlerRoutes);
 router.use('/search-alerts', searchAlertRoutes);
 router.use('/tcdd', tcddRoutes);
 router.use('/contact', contactRoutes);
+router.use('/healthcheck', healthcheckRoutes);
 
 export default router; 
